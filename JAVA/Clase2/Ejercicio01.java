5f489ce8d4be688b957e2183829ae7f070520773
FranciscoKnap <franciscoknap@gmail.com>
2025-09-01 15:51:48 -0300

    Se realizaron las tareas de la clase 2 de java y se movio a la carpeta clase 1 el archivo de cicloswhile que estaba fuera de lugar

---
+++ JAVA/Clase2/Ejercicio01.java
@@ -0,0 +1,18 @@
+import javax.swing.JOptionPane;
+
+public class Ejercicio01 {
+    public static void main(String[] args) {
+        int numero, cuadrado;
+
+        numero = Integer.parseInt(JOptionPane.showInputDialog("Digite un número: "));
+
+        while (numero >= 0) { // Mientras el número sea igual a cero o positivo
+            cuadrado = (int) Math.pow(numero, 2);
+            System.out.println("El numero " + numero + " elevado al cuadrado es: " + cuadrado);
+
+            numero = Integer.parseInt(JOptionPane.showInputDialog("Digite otro número: "));
+        }
+
+        System.out.println("El programa ha finalizado por número negativo");
+    }
+}
---
+++ JAVA/Clase2/Ejercicio02JOptionPane.java
@@ -0,0 +1,19 @@
+import javax.swing.JOptionPane;
+
+public class Ejercicio02JOptionPane {
+    public static void main(String[] args) {
+        int numero = Integer.parseInt(JOptionPane.showInputDialog("Digite un número: "));
+
+        while (numero != 0) {
+            if (numero > 0) {
+                JOptionPane.showMessageDialog(null, "El número " + numero + " es POSITIVO");
+            } else {
+                JOptionPane.showMessageDialog(null, "El número " + numero + " es NEGATIVO");
+            }
+
+            numero = Integer.parseInt(JOptionPane.showInputDialog("Digite otro número: "));
+        }
+
+        JOptionPane.showMessageDialog(null, "El número " + numero + " finaliza el programa");
+    }
+}
