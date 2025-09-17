4377e46e248df374d29872a41ba6f41d2cb069e8
Franco279 <frnccala@gmail.com>
2025-09-17 10:04:03 -0300

    Add files via upload

---
+++ Python/Clase6/Atributos en metodos y creacion de un objeto.py
@@ -0,0 +1,10 @@
+class Persona: #Creamos una clase
+    def __init__(self):
+        self .nombre = 'Juan'
+        self .apellido = 'Salazar'
+        self .edad = '22'
+
+persona1 = Persona()
+print(persona1.nombre)
+print(persona1.apellido)
+print(persona1.edad)
