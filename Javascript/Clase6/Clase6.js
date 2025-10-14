3fe533168e4e5efec5ac0542ffd83d394ebeb4b0
FranciscoKnap <franciscoknap@gmail.com>
2025-10-13 21:53:04 -0300

    Se agrego la clase 6 de javascript

---
+++ Javascript/Clase6/Clase6.js
@@ -0,0 +1,46 @@
+class Persona{  //Clase padre
+    constructor(nombre, apellido){
+        this._nombre = nombre;
+        this._apellido = apellido;
+    }
+    get nombre(){
+        return this._nombre;
+    }
+    get apellido(){
+        return this._apellido;
+    }
+
+    set nombre(nombre){
+        this._nombre = nombre;
+    }
+    set apellido(apellido){
+        this._apellido = apellido;
+    }
+
+}
+
+class Empleado extends Persona{ //Clase hija
+    constructor(nombre, apellido, departamento){
+        super(nombre, apellido);
+        this._departamento = departamento;
+    }
+    get departamento(){
+        return this._departamento;
+    }
+    set departamento(departamento){
+        this._departamento = departamento;
+    }
+}
+
+let persona1 = new Persona('Martin', 'Perez');
+console.log(persona1.nombre);
+persona1.nombre = 'Juan Carlos';
+persona1.apellido = 'Alvarez';
+
+let persona2 = new Persona('Carlos', 'Lara');
+console.log(persona2.nombre);  
+persona2.nombre = 'María Laura';
+console.log(persona2.nombre); 
+
+let empleado1 = new Empleado('María', 'Gimenez', 'Sistemas');
+console.log(empleado1);
