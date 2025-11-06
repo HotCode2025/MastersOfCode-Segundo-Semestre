19f30438de02f71d72c620ed871ea72ff5254d55
FranciscoKnap <franciscoknap@gmail.com>
2025-11-06 16:33:54 -0300

    Subida inicial proyecto integrador - Se creo Matriz de cps

---
+++ Proyecto-integrador/PRINT3D/src/main/java/com/mycompany/print3d/PRINT3D.java
@@ -0,0 +1,31 @@
+
+package com.mycompany.print3d;
+
+public class PRINT3D {
+
+    public static void main(String[] args) {
+        System.out.println("BIENVENIDOS AL MENU DE PAGO Y ENVIOS DE PRINT3D");
+        
+        //Matriz con los codigos postales de los 18 departamentos de Mendoza
+        int[][] matriz = new int[6][3];
+
+        matriz[0][0] = 5500;
+        matriz[0][1] = 5501;
+        matriz[0][2] = 5521;
+        matriz[1][0] = 5539;
+        matriz[1][1] = 5507;
+        matriz[1][2] = 5515;
+        matriz[2][0] = 5620;
+        matriz[2][1] = 5570;
+        matriz[2][2] = 5590;
+        matriz[3][0] = 5531;
+        matriz[3][1] = 5613;
+        matriz[3][2] = 5577;
+        matriz[4][0] = 5569;
+        matriz[4][1] = 5570;
+        matriz[4][2] = 5600;
+        matriz[5][0] = 5560;
+        matriz[5][1] = 5560;
+        matriz[5][2] = 5561;
+    }
+}
