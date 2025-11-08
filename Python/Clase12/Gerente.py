29590a9ec17aee9e73f59a0f009ea49836e4d534
JoseBr2004 <joseignaciobritosperso@gmail.com>
2025-11-08 03:28:02 -0300

    Clase 12 poliformio

---
+++ Python/Clase12/Gerente.py
@@ -0,0 +1,7 @@
+class Gerente(Empleado):
+    def __init__(self, self, nombre, sueldo, departamento):
+        super().__init__(nombre, sueldo)
+        self.departamento = departamento
+
+    def __str__(self):
+        return f'Gerente [ Departamento: {self.departamento} ] {super().__str__()} '
