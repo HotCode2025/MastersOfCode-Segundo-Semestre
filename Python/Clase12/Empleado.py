8543f69b3aed89778352b8caaac015b26903f5f1
TapiaXimena <x.tapia92@outlook.com>
2025-11-08 07:52:14 -0300

    Clase 12 - Polimorfismo - Actualizado

--- Python/Clase12/Empleado.py
+++ Python/Clase12/Empleado.py
@@ -1,3 +1,5 @@
+class Empleado: # No hereda sino solo de la clase object
+
     def __init__(self, nombre, sueldo):
         self.nombre = nombre
         self.sueldo = sueldo
--- Python/Clase12/Gerente.py
+++ Python/Clase12/Gerente.py
@@ -1,4 +1,7 @@
+from Empleado import Empleado
+
 class Gerente(Empleado):
+    def __init__(self, nombre, sueldo, departamento):
         super().__init__(nombre, sueldo)
         self.departamento = departamento
 
