8a95526ad83bbeaf92f7c2ab26f7e426df5c31f3
Emiliano Bogado <emilianobogado2004@gmail.com>
2025-11-08 15:25:26 -0300

    Clase 4 portafolio grupal

--- GITHUB/PORTAFOLIO CLASE 1/src/App.vue
+++ GITHUB/PORTAFOLIO CLASE 1/src/App.vue
@@ -1,20 +1,10 @@
 <script setup>
-
+import NavBar from './components/NavBar.vue';
 </script>
 
 <template>
   <header>
-    <!--barra de navegacion-->
-    <nav class="navbar">
-      <div class="navbar-menu">
-        <ul>
-          <li><a href="#educacion" class="navbar-item">Educacion</a></li>
-          <li><a href="#experiencia" class="navbar-item">Experiencia</a></li>
-          <li><a href="#proyectos" class="navbar-item">Proyecto</a></li>
-          <li><a href="#habilidades" class="navbar-item">Habilidades</a></li>
-        </ul>
-      </div> 
-    </nav>  
+     <NavBar />
   </header>
   <main>
     <section id="educacion"><h2>Educacion - Cursos</h2></section>
@@ -26,46 +16,5 @@
 </template>
 
 <style scoped>
-.navbar {
-  background-color: var(--vt-c-indigo);
-  color: #fff;
-  padding: 0.5rem 1rem;
-  align-items: center;
-}
-.navbar-item {
-  color: #fff;
-  text-decoration: none;
-  margin-right: 1rem;
-}
-.navbar-menu {
-  display: flex;
-  justify-content: flex-end;
-}
-.nav-list {
-  list-style: none;
-}
-
-a {
-  border: 1px solid;
-  border-color: hsla(100, 100%, 37%, 0.2);
-  border-radius: 5px;
-  /*elimina lo subrayado de elementos y elementos con clase "green"*/
-  text-decoration: none;
-  /*añade una transicion suave para los cambios de estilo*/
-  transition: 0.4s;
-  /*añade padding alrededor del contenido*/
-  padding: 5px;
-}
-a :hover{
-  background-color: hsla(160, 100%, 37%, 0.2)
-}
-
-@media (max-width: 768px) {
-  .navbar-menu{
-    display: flex;
-    justify-content: flex-end;
-    width: 100%;
-  }
-}
 
 </style>
